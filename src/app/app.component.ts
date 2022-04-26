d4b4549d35865a370b586d2fbe34456fa0754149
Oleg Gladyshev <molfarr@gmail.com>
2022-04-26 15:00:00 +0200

    Create Angular CLI app

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+//https://www.bankofcanada.ca/valet/observations/FXAUDCAD,FXCNYCAD,FXEURCAD,FXJPYCAD,FXMXNCAD,FXNZDCAD,FXSEKCAD,FXCHFCAD,FXGBPCAD,FXUSDCAD/json?start_date=2017-01-03
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'MarshallZehr-Coding-Check';
+}
